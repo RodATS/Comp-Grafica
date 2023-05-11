e0be1a1ae0fb4653338ae3b667ec18edc0bd0a04
RodATS <77297145+RodATS@users.noreply.github.com>
2023-05-11 18:44:09 -0500

    Add files via upload

---
+++ OpenGL/Project_08_Transformaciones/transf.h
@@ -0,0 +1,156 @@
+#ifndef TRANSF_H
+#define TRANSF_H
+
+#include <iostream>
+#include <math.h>
+
+//-----------------CLASE Transformaciones-------------------------------------------------------------
+struct Transformaciones {
+    float rot_x[4][4], rot_y[4][4], rot_z[4][4], T[4][4], S[4][4];
+
+    //Solo inicializa las matrices
+    Transformaciones() {
+        float temp[4][4] = { {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,1} };
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                rot_x[i][j] = temp[i][j];
+                rot_y[i][j] = temp[i][j];
+                rot_z[i][j] = temp[i][j];
+                T[i][j] = temp[i][j];
+                S[i][j] = temp[i][j];
+            }
+        }
+    }
+
+    float* multiplicacion(float matriz1[3], float matriz2[4][4], bool vector) {
+        float temp[4][1];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 1; j++) {
+                temp[i][j] = 0;
+            }
+        }
+
+        float new_matriz1[4][1];
+        for (int i = 0; i < 3; i++) {
+            new_matriz1[i][0] = matriz1[i];
+        }
+        if (!vector) {
+            new_matriz1[3][0] = 0;
+        }
+        else {
+            new_matriz1[3][0] = 1;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 1; j++) {
+                for (int k = 0; k < 4; k++) {
+                    temp[i][j] += matriz2[i][k] * new_matriz1[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            matriz1[i] = temp[i][0];
+        }
+
+        return matriz1;
+    }
+
+    //Rotacion en x
+    void Rotacion_x(float angle, float* &vertices, int size) {
+        //Convertimos a grados
+        float degree = angle * (3.141592653589793238463 / 180);
+        float temp[3];
+        float* res = new float[3];
+        //Construimos la matrix rotacion_x con el angulo 
+        rot_x[0][0] = 1;
+        rot_x[1][1] = cos(degree); rot_x[1][2] = -sin(degree);
+        rot_x[2][1] = sin(degree); rot_x[2][2] = cos(degree);
+        //Multiplicamos por todos los vectores recibidos y actualizamos nuestra matriz
+        for (int i = 0; i < size; i += 3) {
+            temp[0] = vertices[i];
+            temp[1] = vertices[i + 1];
+            temp[2] = vertices[i + 2];
+            res = multiplicacion(temp, rot_x, 0);
+            vertices[i] = res[0];
+            vertices[i + 1] = res[1];
+            vertices[i + 2] = res[2];
+        }
+    }
+
+    void Rotacion_y(float angle, float* &vertices, int size) {
+        float degree = angle * (3.141592653589793238463 / 180);
+        float temp[3];
+        float* res = new float[3];
+        rot_y[0][0] = cos(degree); rot_y[0][2] = sin(degree);
+        rot_y[1][1] = 1;
+        rot_y[2][0] = -sin(degree); rot_y[2][2] = cos(degree);
+        for (int i = 0; i < size; i += 3) {
+            temp[0] = vertices[i];
+            temp[1] = vertices[i + 1];
+            temp[2] = vertices[i + 2];
+            res = multiplicacion(temp, rot_y, 0);
+            vertices[i] = res[0];
+            vertices[i + 1] = res[1];
+            vertices[i + 2] = res[2];
+        }
+    }
+
+    void Rotacion_z(float angle, float* &vertices, int size) {
+        float degree = angle * (3.141592653589793238463 / 180);
+        float temp[3];
+        float* res = new float[3];
+        rot_z[0][0] = cos(degree); rot_z[0][1] = -sin(degree);
+        rot_z[1][0] = sin(degree); rot_z[1][1] = cos(degree);
+        rot_z[2][2] = 1;
+        for (int i = 0; i < size; i += 3) {
+            temp[0] = vertices[i];
+            temp[1] = vertices[i + 1];
+            temp[2] = vertices[i + 2];
+            res = multiplicacion(temp, rot_z, 0);
+            vertices[i] = res[0];
+            vertices[i + 1] = res[1];
+            vertices[i + 2] = res[2];
+        }
+    }
+
+    void Translation(float tx, float ty, float tz, float* &vertices, int size) {
+        float temp[3];
+        float* res = new float[3];
+        T[0][0] = 1; T[0][3] = tx;
+        T[1][1] = 1; T[1][3] = ty;
+        T[2][2] = 1; T[2][3] = tz;
+        for (int i = 0; i < size; i += 3) {
+            temp[0] = vertices[i];
+            temp[1] = vertices[i + 1];
+            temp[2] = vertices[i + 2];
+            res = multiplicacion(temp, T, 1);
+            vertices[i] = res[0];
+            vertices[i + 1] = res[1];
+            vertices[i + 2] = res[2];
+        }
+    }
+
+    void Scale(float sx, float sy, float sz, float* &vertices, int size) {
+        float temp[3];
+        float* res = new float[3];
+        S[0][0] = sx;
+        S[1][1] = sy;
+        S[2][2] = sz;
+        for (int i = 0; i < size; i += 3) {
+            temp[0] = vertices[i];
+            temp[1] = vertices[i + 1];
+            temp[2] = vertices[i + 2];
+            res = multiplicacion(temp, S, 0);
+            vertices[i] = res[0];
+            vertices[i + 1] = res[1];
+            vertices[i + 2] = res[2];
+        }
+    }
+};
+
+//-----------------CLASE Transformaciones-------------------------------------------------------------
+
+
+
+#endif
